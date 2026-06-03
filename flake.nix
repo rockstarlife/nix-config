5a49350535e23fd7b263e20779ccf91c8fb05184
rockstarlife <chye.stas.stanislav@gmail.com>
2026-06-03 21:57:26 +0300

    first updated 26.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   description = "my NixOS setup, kiss and declarative";
 
 inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # nix-cachyos-kernel = {
